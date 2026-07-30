8de8b10f63246f0b63ecb289c1624454a22e60b1
Patrick List <patrick.list@pm.me>
2026-07-30 20:02:53 +0200

    remove eslint + begin auth

---
+++ backend/prisma/migrations/20260730175610_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserIdentity" (
+    "providerName" TEXT NOT NULL,
+    "providerSub" TEXT NOT NULL,
+    "userId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserIdentity_providerName_providerSub_key" ON "UserIdentity"("providerName", "providerSub");
+
+-- AddForeignKey
+ALTER TABLE "UserIdentity" ADD CONSTRAINT "UserIdentity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
